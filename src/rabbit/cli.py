c63b7515436672c051e87861e02596fd74899ca5
Cyril Moreau <cyril.moreau07@proton.me>
2025-12-01 13:29:32 +0100

    REF: support for ghmap and new project structure (WIP)
    - Split predictor and extractor (events)
    - Add tests (predict sample user from events and load models)
    - ghmap is now used as the activity mapping
    - usage of uv
    
    Mostly WIP, extractor and CLI not yet reimplemented but predictor works.

---
+++ src/rabbit/cli.py
@@ -0,0 +1,7 @@
+
+
+def main():
+    print("Rabbit CLI")
+
+if __name__ == '__main__':
+    main()
---
+++ tests/tests_predictor/test_predictor.py
@@ -0,0 +1,21 @@
+import os
+
+def _load_events(file_path):
+    import json
+
+    with open(file_path, "r", encoding="utf-8") as f:
+        events = json.load(f)
+    return events
+
+def test_predict_human():
+    from rabbit.predictor import predict_user_type
+
+
+    events_file = os.path.join(os.path.dirname(__file__), "..","data", "events_human.json")
+    user_events = _load_events(events_file)
+
+    username = "test"
+    user_type, confidence = predict_user_type(username, user_events)
+    assert user_type == "Human"
+    assert 0.0 <= confidence <= 1.0
+
